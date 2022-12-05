44725b4126e1abd8fc7452bd24dd6dec9daf453d
Charan <itscharanteja789@gmail.com>
2022-12-05 19:26:27 +0530

    shell scripts

---
+++ ncr.sh
@@ -0,0 +1,25 @@
+function fact {
+     mul=1
+     i=$1
+     while [[ $i -gt 1 ]]
+     do
+     sub=`expr $i - 1`
+     mul=`expr $mul \* $i`
+     i=$sub
+     done
+     echo $mul
+}
+
+echo "Enter value for n"
+n= read n
+a=`fact $n`
+echo "Enter Value for r"
+r= read r
+b=`fact $r`
+c=`expr "$n" - "$r"`
+d=`fact $c`
+t=`expr "$b" \* "$d"`
+ans=`expr "$a" / "$t"`
+
+echo "Answer For $n! / ($n - $r)!($r)! is"
+echo "$ans"
---
+++ test.sh
@@ -0,0 +1,8 @@
+echo "Enter value for n"
+n= read n
+a=`fact $n`
+echo "Enter Value for r"
+r= read r
+b=`fact $r`
+
+echo "$a,$b"
