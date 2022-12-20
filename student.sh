ef1db4d1aad77f8cc7c02d88d80e6c0e149c04f9
Charan <itscharanteja789@gmail.com>
2022-12-20 22:18:37 +0530

    .

---
+++ student.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Enter student number:"
+read snum
+
+echo "Enter student name:"
+read sname
+
+echo "Enter marks in 5 subjects:"
+read m1 m2 m3 m4 m5
+
+total=$((m1 + m2 + m3 + m4 + m5))
+avg=$((total/5))
+
+if [ $avg -ge 80 ]
+then
+    grade="A"
+elif [ $avg -ge 70 ]
+then
+    grade="B"
+elif [ $avg -ge 60 ]
+then
+    grade="C"
+elif [ $avg -ge 50 ]
+then
+    grade="D"
+elif [ $avg -ge 40 ]
+then
+    grade="E"
+else
+    grade="F"
+fi
+
+echo "Student number: $snum"
+echo "Student name: $sname"
+echo "Total marks: $total"
+echo "Average: $avg"
+echo "Grade: $grade"
